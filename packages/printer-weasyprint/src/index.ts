235b95111095839c5725749ebc02054514ed5d72
Niels van Velzen <git@ndat.nl>
2021-01-24 12:21:39 +0100

    Install WeasyPrint in CI

--- packages/printer-weasyprint/src/index.ts
+++ packages/printer-weasyprint/src/index.ts
@@ -1,28 +1,43 @@
 import { PluginValues, ContentProvider, ContentRenderContext, PrinterProvider, PrinterRenderContext, PrinterSource } from '@doctool/plugin-api';
-import HtmlPrinterPlugin, { HtmlPrinterProvider } from '@doctool/printer-html';
+import HtmlPrinterPlugin, { HtmlPrinterProvider, HtmlPrinterProviderData } from '@doctool/printer-html';
 import childProcess from 'child_process';
 
-export class WeasyprintPrinterProvider implements PrinterProvider {
+export interface WeasyPrintPrinterProviderData extends HtmlPrinterProviderData {
+	weasyprint?: string
+}
+
+export class WeasyPrintPrinterProvider implements PrinterProvider {
 	private readonly htmlPrinterProvider: HtmlPrinterProvider;
 
 	constructor(htmlPrinterProvider: HtmlPrinterProvider) {
 		this.htmlPrinterProvider = htmlPrinterProvider;
 	}
 
-	async render<T extends object>(context: PrinterRenderContext, sources: PrinterSource[], data: T): Promise<Buffer> {
+	async render(context: PrinterRenderContext, sources: PrinterSource[], data: WeasyPrintPrinterProviderData): Promise<Buffer> {
 		const html = await this.htmlPrinterProvider.render(context, sources, data);
+		const bin = data.weasyprint || 'weasyprint';
 	
 		return new Promise((resolve, reject) => {
-			const process = childProcess.spawn('weasyprint', ['-f', 'pdf', '-e', 'utf-8', '-', '-']);
+			const options = [
+				// Format
+				'-f', 'pdf',
+				// Encoding
+				'-e', 'utf-8',
+				// Input (stdin)
+				'-',
+				// Output (stdout)
+				'-'
+			];
+			const process = childProcess.spawn(bin, options);
 
 			const buffers: Buffer[] = [];
 			process.stdout.on('data', chunk => buffers.push(chunk));
 			process.stderr.pipe(process.stderr as any);
 			process.on('close', () => resolve(Buffer.concat(buffers)));
 			process.on('error', err => reject(err));
-
-			process.stdin.write(html, 'utf-8');
-			process.stdin.end();
+			
+			process.stdin.on('error', err => reject(err));
+			process.stdin.end(html, 'utf-8');
 		});
 	}
 }
@@ -33,7 +48,7 @@ export default async function (): Promise<PluginValues> {
 		
 	return {
 		printerProviders: {
-			'weasyprint': new WeasyprintPrinterProvider(htmlPrinterProvider)
+			'weasyprint': new WeasyPrintPrinterProvider(htmlPrinterProvider)
 		}
 	}
 }

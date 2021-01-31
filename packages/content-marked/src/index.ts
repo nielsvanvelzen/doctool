8c06d32d0558e5a11b8cc11d7e24ec1c262987c3
Niels van Velzen <git@ndat.nl>
2021-01-31 22:52:52 +0100

    Fix assets in content-marked

--- packages/content-marked/src/index.ts
+++ packages/content-marked/src/index.ts
@@ -4,7 +4,7 @@ import marked from 'marked';
 export class MarkedContentProvider implements ContentProvider {
 	async render<T extends object>(context: ContentRenderContext, location: string, source: Buffer, data: T): Promise<Buffer> {
 		const options: marked.MarkedOptions = {
-			baseUrl: location
+			baseUrl: `file:///${context.resolvePath('asset', '.')}`,
 		};
 
 		const result: string = await new Promise((resolve, reject) => {

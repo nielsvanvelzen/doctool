8e99e52c171ae2fd88a404183e912154764192dc
Niels van Velzen <git@ndat.nl>
2021-02-09 15:01:15 +0100

    SCSS support in html printer

--- packages/printer-html/src/index.ts
+++ packages/printer-html/src/index.ts
@@ -1,5 +1,6 @@
 import { PluginValues, PrinterProvider, PrinterRenderContext, PrinterSource } from '@doctool/plugin-api';
 import prettier from 'prettier';
+import sass from 'sass';
 import url from 'url';
 import escapeHtml from 'escape-html';
 
@@ -7,22 +8,32 @@ export interface HtmlPrinterProviderData {
 	title?: string,
 	css?: string[] | string
 }
+
 export class HtmlPrinterProvider implements PrinterProvider {
 	readonly defaultExtension: string = 'html';
 
-	private renderLayout(title: string|null, css: string[], body: string): string {
+	private async renderCss(location: string): Promise<string> {
+		if (location.endsWith('.scss')) {
+			const result = sass.renderSync({
+				file: location
+			});
+
+			return `<style>${result.css}</style>`;
+		}
+
+		return `<link rel="stylesheet" href="${url.pathToFileURL(location)}" />`;
+	}
+
+	private async renderLayout(title: string|null, css: string[], body: string): Promise<string> {
 		let head = '';
+
 		if (title) head += `<title>${escapeHtml(title)}</title>`;
-		for (const href of css) head += `<link rel="stylesheet" href="${url.pathToFileURL(href)}" />`;
+		for (const href of css) head += await this.renderCss(href);
 
 		return `<!DOCTYPE html>
 			<html>
-			<head>
-				${head}
-			</head>
-			<body>
-				${body}
-			</body>
+			<head>${head}</head>
+			<body>${body}</body>
 			</html>
 		`;
 	}
@@ -32,6 +43,7 @@ export class HtmlPrinterProvider implements PrinterProvider {
 			parser: 'html',
 			endOfLine: 'auto'
 		};
+		
 		return prettier.format(source, options);
 	}
 
@@ -39,7 +51,7 @@ export class HtmlPrinterProvider implements PrinterProvider {
 		const body = sources.map(source => source.content.toString()).join('');
 		const css = (typeof data.css == 'string' ? [data.css] : Array.isArray(data.css) ? data.css : []).map(href => context.resolvePath('asset', href));
 
-		let html = this.renderLayout(data.title ?? null, css, body);
+		let html = await this.renderLayout(data.title ?? null, css, body);
 		html = this.format(html);
 
 		return Buffer.from(html, 'utf-8');

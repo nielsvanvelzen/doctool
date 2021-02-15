8aa85d7fee68ce615234586a1cc16449b9dd806f
Niels van Velzen <git@ndat.nl>
2021-02-15 14:09:21 +0100

    Add second markdown plugin based on remark

---
+++ packages/content-remark/src/index.ts
@@ -0,0 +1,31 @@
+import { PluginValues, ContentProvider, ContentRenderContext } from '@doctool/plugin-api';
+import HtmlContentPlugin, { HtmlContentProvider } from '@doctool/content-html';
+import remark from 'remark';
+import remarkHtml from 'remark-html';
+
+export class RemarkContentProvider implements ContentProvider {
+	private readonly htmlContentProvider: HtmlContentProvider;
+
+	constructor(htmlContentProvider: HtmlContentProvider) {
+		this.htmlContentProvider = htmlContentProvider;
+	}
+
+	async render<T extends object>(context: ContentRenderContext, location: string, source: Buffer, data: T): Promise<Buffer> {
+		const processor = remark();
+		processor.use(remarkHtml);
+		const result = await processor.process(source);
+
+		return await this.htmlContentProvider.render(context, location, Buffer.from(String(result), 'utf-8'), data);
+	}
+}
+
+export default async function(): Promise<PluginValues> {
+	const htmlContentPlugin = await HtmlContentPlugin();
+	const htmlContentProvider = htmlContentPlugin.contentProviders!['.html'] as HtmlContentProvider;
+
+	return {
+		contentProviders: {
+			'.md': new RemarkContentProvider(htmlContentProvider)
+		}
+	}
+}

677f49d1bad96450dff2dd048181b2d7c395e908
Niels van Velzen <git@ndat.nl>
2021-02-15 13:53:28 +0100

    Improve src/href attribute rewriting

--- packages/content-html/src/index.ts
+++ packages/content-html/src/index.ts
@@ -1,10 +1,36 @@
 import { ContentProvider, PluginValues, ContentRenderContext } from '@doctool/plugin-api';
 import RewritingStream from 'parse5-html-rewriting-stream';
+import path from 'path';
+import fs from 'fs';
+import url, { URL } from 'url';
 
 const rewriteHref = ['a', 'area', 'base', 'link'];
 const rewriteSrc = ['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video'];
 
 export class HtmlContentProvider implements ContentProvider {
+	resolveUrl(context: ContentRenderContext, origin: string, src: string): string {
+		// Hash
+		if (src.startsWith('#')) return src;
+
+		try {
+			// Validate current format
+			return new URL(src).href;
+		} catch (err) {
+			// Invalid URL, probably relative so ignore!
+		}
+
+		const relativePath = path.resolve(origin, src);
+		if (fs.existsSync(relativePath)) return url.pathToFileURL(relativePath).href;
+
+		try {
+			const assetPath = context.resolvePath('asset', src);
+			return url.pathToFileURL(assetPath).href;
+		} catch (err) {
+			console.error(`Could not resolve path ${src} from ${origin}.`);
+			return src;
+		}
+	}
+
 	async render<T extends object>(context: ContentRenderContext, location: string, source: Buffer, data: T): Promise<Buffer> {
 		const rewriter = new RewritingStream();
 		const resultPromise: Promise<Buffer> = new Promise((resolve, reject) => {
@@ -18,15 +44,13 @@ export class HtmlContentProvider implements ContentProvider {
 		rewriter.on('startTag', tag => {
 			if (rewriteHref.includes(tag.tagName)) {
 				for (const attr of tag.attrs) {
-					if (attr.name == 'href')
-						attr.value = new URL(`file:///${context.resolvePath('asset', context.resolvePath('asset', attr.value))}`).href;
+					if (attr.name == 'href') attr.value = this.resolveUrl(context, location, attr.value);
 				}
 			}
 			
 			if (rewriteSrc.includes(tag.tagName)) {
 				for (const attr of tag.attrs) {
-					if (attr.name == 'src')
-						attr.value = new URL(`file:///${context.resolvePath('asset', context.resolvePath('asset', attr.value))}`).href;
+					if (attr.name == 'src') attr.value = this.resolveUrl(context, location, attr.value);
 				}
 			}
 			
